e93227f181dda56137e8f5a36d1b0da10555fb53
leisure-play <1536841051@qq.com>
2025-06-04 23:02:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx06585f7783d0e864",
   appsecret: "c5e208309fae97221bc96af39d101344",
-  template_id: "V9VvFNU5Ca585r01CW8AtNdNu2jRqNBb3TO4fGfTuC0",
+  template_id: "_cAi5Mv_9kaSqCk22_rIUBeYzPtP9Zj_-wlJGx351T0",
   touser: "odPTA7Vy9zD57bsgFXSkRmVrRD_I",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
